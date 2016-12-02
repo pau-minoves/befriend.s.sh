ca45dd151d7e7400a70298b3c7bd35afc5a84edf
Pau Minoves <pau.minoves@gmail.com>
2016-12-02 12:44:25 +0100

    Change DSA for RSA, add -v and ssh.shortcut generation

--- befriend.s.sh
+++ befriend.s.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+SCRIPT_DIR=$(dirname "$0")
 REMOTEHOST=$1
 
 if test -z $REMOTEHOST
@@ -13,24 +14,19 @@ if test	-f ~/.ssh/identity.pub
 then
 	SSHKEY=~/.ssh/identity.pub
 else
-	if test -f ~/.ssh/id_dsa.pub
+	if test -f ~/.ssh/id_rsa.pub
 	then
-		SSHKEY=~/.ssh/id_dsa.pub
+		SSHKEY=~/.ssh/id_rsa.pub
 	else
-		if test -f ~/.ssh/id_rsa.pub
-		then
-			SSHKEY=~/.ssh/id_rsa.pub
-		else
-			echo No available key found, creating one
-			ssh-keygen -t dsa
-		fi
+		echo No available key found, creating one
+		ssh-keygen -t rsa
 	fi
 fi
 
 echo Using ${SSHKEY}
 echo Connecting to ${REMOTEHOST}
 
-cat ${SSHKEY} | ssh ${REMOTEHOST} 'cat >> /tmp/sshkey.pub;
+cat ${SSHKEY} | ssh -v ${REMOTEHOST} 'cat >> /tmp/sshkey.pub;
 					mkdir -p ~/.ssh;
 					cat /tmp/sshkey.pub >> ~/.ssh/authorized_keys;
 					cat /tmp/sshkey.pub >> ~/.ssh/authorized_keys2;
@@ -38,3 +34,7 @@ cat ${SSHKEY} | ssh ${REMOTEHOST} 'cat >> /tmp/sshkey.pub;
 					chmod 640 ~/.ssh/authorized_keys
 					chmod 640 ~/.ssh/authorized_keys2
 				       	rm /tmp/sshkey.pub' && echo Done!
+
+echo "Creating ssh.${REMOTEHOST} shortcut"
+echo "ssh ${REMOTEHOST}" >> ${SCRIPT_DIR}/ssh.${REMOTEHOST}
+chmod u+x ${SCRIPT_DIR}/ssh.${REMOTEHOST}
